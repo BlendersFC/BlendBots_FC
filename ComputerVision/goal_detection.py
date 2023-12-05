102d66e61d52f804e5c634b93c7b8786264e8382
Iker Casillas <ikercasillasp2@gmail.com>
2023-12-05 11:32:52 -0600

    Grass border detection & white bg test case

--- ComputerVision/goal_detection.py
+++ ComputerVision/goal_detection.py
@@ -2,16 +2,38 @@ import cv2
 import numpy as np
 import os
 
-def divide_image(image, rows, cols):
+def divide_image(image, rows, cols, top_border):
     # Get image dimensions
     height, width = image.shape[:2]
 
     # Calculate the height and width of each grid cell
-    cell_height = height // rows
+    cell_height = (height - top_border) // rows
     cell_width = width // cols
 
-    # Divide the image into a grid of cells
-    cells = [image[i * cell_height:(i + 1) * cell_height, j * cell_width:(j + 1) * cell_width] for i in range(rows) for j in range(cols)]
+    # Divide the image into a grid of cells, starting from the top_border
+    cells = [
+        image[
+            top_border + i * cell_height: top_border + (i + 1) * cell_height,
+            j * cell_width: (j + 1) * cell_width,
+        ]
+        for i in range(rows)
+        for j in range(cols)
+    ]
+
+    #  # Create an image to visualize the cells
+    # cell_visualization = np.zeros_like(image)
+
+    # # Draw rectangles around each cell
+    # for i in range(rows):
+    #     for j in range(cols):
+    #         x1 = j * cell_width
+    #         y1 = top_border + i * cell_height
+    #         x2 = (j + 1) * cell_width
+    #         y2 = top_border + (i + 1) * cell_height
+    #         cv2.rectangle(cell_visualization, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    # # Display the visualization
+    # cv2.imshow("Cell Visualization", cell_visualization)
 
     return cells
 
@@ -27,9 +49,11 @@ def calculate_light_pixel_concentration(image_part):
 
     return light_pixel_concentration
 
-def find_light_interest_regions(image, rows, cols, num_regions):
+def find_light_interest_regions(image, rows, cols, num_regions, top_border):
     # Divide the image into a grid of cells
-    image_cells = divide_image(image, rows, cols)
+    image_cells = divide_image(image, rows, cols, top_border)
+
+    # print(len(image_cells))
 
     # Calculate light pixel concentration for each cell
     concentrations = [calculate_light_pixel_concentration(cell) for cell in image_cells]
@@ -37,12 +61,76 @@ def find_light_interest_regions(image, rows, cols, num_regions):
     # Find the indices of the top N cells with the highest light pixel concentration
     top_indices = np.argsort(concentrations)[-num_regions:]
 
+      # Display the top regions with rectangles
+    display_top_regions(image, rows, cols, top_indices, top_border)
+
     return top_indices, [image_cells[i] for i in top_indices]
+
+def find_top_border(image):
+    # Convert the image to HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define the lower and upper bounds for the green color
+    lower_green = np.array([32, 112, 16])  # Adjust as needed
+    upper_green = np.array([101, 255, 255])
+
+    # Threshold the image to extract the green color
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+
+     # Display the green mask for visualization
+    cv2.imshow("Green Mask", green_mask)
+
+    # Find contours in the green mask
+    contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        # Find the contour with the minimum y-coordinate (top border)
+        min_y = min(contours, key=lambda c: cv2.boundingRect(c)[1])[0][0][1]
+        return min_y
+    else:
+        return 0  # Default to 0 if no contours are foun
+
+
+# Debug functions
+def display_top_regions(image, rows, cols, top_indices, top_border):
+    # Draw rectangles around the top regions for visualization
+    for index in top_indices:
+        i, j = divmod(index, cols)
+
+        # Calculate the coordinates in the original image
+        x = j * (image.shape[1] // cols)
+        y = top_border + i * (image.shape[0] - top_border) // rows
+        w = image.shape[1] // cols
+        h = (image.shape[0] - top_border) // rows
+
+        # Draw the rectangle
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+def draw_contours(image, contours, index, num_cols, num_rows, top_border):
+    offset_x = (index % num_cols) * (image.shape[1] // num_cols)
+    offset_y = top_border + (index // num_cols) * (image.shape[0] - top_border) // num_rows
+    
+    # Draw the contours on the original image with correct offset
+    for contour in contours:
+        contour_with_offset = contour + (offset_x, offset_y)
+        cv2.drawContours(image, [contour_with_offset], -1, (0, 0, 255), 2)
+
+def calculate_centers(contours, index, num_cols, num_rows, top_border, goal_centers, image):
+    offset_x = (index % num_cols) * (image.shape[1] // num_cols)
+    offset_y = top_border + (index // num_cols) * (image.shape[0] - top_border) // num_rows
+    for contour in contours:
+        M = cv2.moments(contour)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            goal_centers.append((cX + offset_x, cY + offset_y))
+
+    return goal_centers
 
 def main():
 
     script_dir = os.path.dirname(__file__) 
-    rel_path = "Img\\goal.jpg"
+    rel_path = "Img\\goal4.jpg"
     image_path = os.path.join(script_dir, rel_path)
     image = cv2.imread(image_path)
 
@@ -53,45 +141,37 @@ def main():
     # Number of top regions to select
     num_top_regions = 8
 
-    # Find the top N regions with the highest light pixel concentration
-    top_indices, top_regions = find_light_interest_regions(image, num_rows, num_cols, num_top_regions)
+    # Find the top border of the grass
+    top_border = find_top_border(image)
+
+    # Display the top border for visualization
+    cv2.line(image, (0, top_border), (image.shape[1], top_border), (0, 0, 255), 2)
+
+    # Find the top N regions with the highest light pixel concentration, discarding the part above the top border
+    top_indices, top_regions = find_light_interest_regions(image, num_rows, num_cols, num_top_regions, top_border)
 
     # Calculate the center of the goal posts
+
     goal_centers = []
     for index, region in zip(top_indices, top_regions):
-        # Convert the region to grayscale
         gray_region = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
-        
-        # Threshold the region to create a binary mask
         _, binary_mask = cv2.threshold(gray_region, 200, 255, cv2.THRESH_BINARY)
-        
-        # Find contours in the binary mask
         contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        # Offset the contours to the correct position in the original image
-        for contour in contours:
-            contour[:, 0, 0] += (index % num_cols) * (image.shape[1] // num_cols)
-            contour[:, 0, 1] += (index // num_cols) * (image.shape[0] // num_rows)
 
-            # Calculate the center of the contour using moments
-            M = cv2.moments(contour)
-            if M["m00"] != 0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                goal_centers.append((cX, cY))
-
-        # Draw the contours on the original image
-        cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+        # Draw the contours on the original image with correct offset
+        draw_contours(image, contours, index, num_cols, num_rows, top_border)
+        goal_centers = calculate_centers(contours, index, num_cols, num_rows, top_border, goal_centers, image)
 
     # Calculate the average center of the goal posts
     if goal_centers:
+        # print(goal_centers)
         avg_center = tuple(np.mean(goal_centers, axis=0, dtype=int))
 
         # Print the average center coordinates
         print(f"Avg Center Coordinates: {avg_center}")
 
         # Draw a marker at the average center
-        cv2.drawMarker(image, avg_center, (0, 0, 255), markerType=cv2.MARKER_CROSS, markerSize=20, thickness=2)
+        cv2.drawMarker(image, avg_center, (255, 0, 255), markerType=cv2.MARKER_CROSS, markerSize=20, thickness=2)
 
         # Display the result
         cv2.imshow("Goal contours and center", image)
---
+++ ComputerVision/hsv_range_calibration.py
@@ -0,0 +1,73 @@
+# Taken from https://stackoverflow.com/a/59906154/19409752
+
+
+import cv2
+import numpy as np
+import os
+
+
+def nothing(x):
+    pass
+
+# image Path
+script_dir = os.path.dirname(__file__) 
+rel_path = "Img\\goal2.jpg"
+image_path = os.path.join(script_dir, rel_path)
+image = cv2.imread(image_path)
+# image = cv2.imread('HSVScale.py')
+
+# Create a window
+cv2.namedWindow('image')
+
+# Create trackbars for color change
+# Hue is from 0-179 for Opencv
+cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+
+# Set default value for Max HSV trackbars
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize HSV min/max values
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+while(1):
+    # Get current positions of all trackbars
+    hMin = cv2.getTrackbarPos('HMin', 'image')
+    sMin = cv2.getTrackbarPos('SMin', 'image')
+    vMin = cv2.getTrackbarPos('VMin', 'image')
+    hMax = cv2.getTrackbarPos('HMax', 'image')
+    sMax = cv2.getTrackbarPos('SMax', 'image')
+    vMax = cv2.getTrackbarPos('VMax', 'image')
+
+    # Set minimum and maximum HSV values to display
+    lower = np.array([hMin, sMin, vMin])
+    upper = np.array([hMax, sMax, vMax])
+
+    # Convert to HSV format and color threshold
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(image, image, mask=mask)
+
+    # Print if there is a change in HSV value
+    if((phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
+        print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax))
+        phMin = hMin
+        psMin = sMin
+        pvMin = vMin
+        phMax = hMax
+        psMax = sMax
+        pvMax = vMax
+
+    # Display result image
+    cv2.imshow('image', result)
+    if cv2.waitKey(50) == 27:
+            break
+
+cv2.destroyAllWindows()
