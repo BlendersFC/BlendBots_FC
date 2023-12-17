f2af8365ab8cf3368ef354284a4639df6f30657a
Vicente Ruiz <vicenntte@outlook.es>
2023-12-17 16:19:09 -0600

    ahora solo ve una pelota
    creo que si bajamos los parámteros del detectMultiScale el OP3 vera muchos balones pero podemos tomar solo el primero como el 'bueno' entonces no importará que tenga muchas detecciones falsas porque siempre detectará el de verdad y solo tomaríamos ese

--- ComputerVision/pelotahaar.py
+++ ComputerVision/pelotahaar.py
@@ -4,9 +4,9 @@ import cv2
 #cap = cv2.VideoCapture(0,cv2.CAP_DSHOW) 
 #cap = cv2.VideoCapture('C:/Users/vicen/Downloads/WIN_20231208_15_17_00_Pro.mp4')
 #cap = cv2.VideoCapture('C:/Users/vicen/Downloads/WIN_20231208_15_16_32_Pro.mp4')
-cap = cv2.VideoCapture('C:/Users/vicen/Downloads/WIN_20231117_11_36_57_Pro (1).mp4')
+cap = cv2.VideoCapture('C:/Users/vicen/Downloads/WIN_20231117_11_36_57_Pro.mp4')
 
-pelota_clas = cv2.CascadeClassifier('C:/Users/vicen/Documents/Git/BlendBots_FC/ComputerVision/cascade1500y300.xml')
+pelota_clas = cv2.CascadeClassifier('C:/documentos/git_ws/BlendBots_FC/ComputerVision/cascade3000y800.xml')
 
 while True:
 	
@@ -14,19 +14,35 @@ while True:
 	frame_r=cv2.resize(frame,(960, 540))
 	gray = cv2.cvtColor(frame_r, cv2.COLOR_BGR2GRAY)
 
+	'''
 	#parece que con estos parámetros va bien pero habrá que ver con el OP3
 	ball = pelota_clas.detectMultiScale(gray,
 	scaleFactor = 6,
 	minNeighbors = 100, minSize=(100,100))
-
+	
 	for (x,y,w,h) in ball:
 		cv2.rectangle(frame_r, (x,y),(x+w,y+h),(0,255,0),2)
 		cx=int((x+(x+w))/2)
 		cy=int((y+(y+h))/2)
 		cv2.circle(frame_r, (cx, cy), radius=5, color=(0, 255, 0), thickness=1)
+	'''
+	'''
+	Este código e
+	'''
+	objects = pelota_clas.detectMultiScale(gray,
+	scaleFactor = 6,
+	minNeighbors = 100, minSize=(100,100))
 
-	cv2.imshow('frame',frame_r)
+	# Si se detectó al menos un objeto, tomar el primer rectángulo
+	if len(objects) > 0:
+		x, y, w, h = objects[0]
+    	# Dibujar el rectángulo alrededor del primer objeto detectado
+		cx=int((x+(x+w))/2)
+		cy=int((y+(y+h))/2)
+		cv2.rectangle(frame_r, (x, y), (x+w, y+h), (255, 0, 0), 2)
+		cv2.circle(frame_r, (cx, cy), radius=5, color=(0, 255, 0), thickness=1)
 	
+	cv2.imshow('frame',frame_r)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 cap.release()
