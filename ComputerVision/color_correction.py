d07caad4f680e039d62ef295fb6d9dfbd3170b2a
Vicente <vicenntte@outlook.es>
2024-03-18 20:23:35 -0600

    added histogram equalization, and opening with morphological transformations
    Code is now on a function that returns 4 images 'balanced_image, balanced_image_gray, equalized_gray, and opening'

--- ComputerVision/color_correction.py
+++ ComputerVision/color_correction.py
@@ -1,16 +1,33 @@
 import cv2
+import numpy as np
 
-# load and resize image
-input_image = cv2.imread("D:/videos crudos/videos_prueba/i5.jpg")
-input_image = cv2.resize(input_image,(960, 540))
-# Create retinex object
-retinex = cv2.xphoto.createSimpleWB()
 
-# Apply white balance
-balanced_image = retinex.balanceWhite(input_image)
+def filter(img):
+    retinex = cv2.xphoto.createSimpleWB()
+    balanced_image = retinex.balanceWhite(img)
+	
+    balanced_image_gray = cv2.cvtColor(balanced_image, cv2.COLOR_BGR2GRAY)
+    equa = cv2.equalizeHist(balanced_image_gray)
 
-# show results
-cv2.imshow('Original Image', input_image)
-cv2.imshow('Balanced Image (Retinex)', balanced_image)
-cv2.waitKey(0)
+    kernel = np.ones((5, 5), np.uint8)  #kernel for morfologic
+    imagen_erosionada = cv2.erode(balanced_image_gray, kernel, iterations=1) #erosion
+    apertura = cv2.dilate(imagen_erosionada, kernel, iterations=1)#dilatation
+    apertura_c = cv2.cvtColor(apertura,cv2.COLOR_GRAY2BGR) #grayscale to BGR
+
+
+    return [balanced_image,balanced_image_gray,equa,apertura_c]
+
+
+
+
+cap = cv2.VideoCapture("D:/videos crudos/videos_prueba/op3_pov.webm")
+while True:
+	
+	ret,frame_final = cap.read()
+	frame_final=filter(frame_final)[3]
+	cv2.imshow('frame',frame_final)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
 cv2.destroyAllWindows()
