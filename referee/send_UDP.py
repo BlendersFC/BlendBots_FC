faf9cb82f507c8a6a9e35a7ea54bb4e555bfefdd
Vicente <vicenntte@outlook.es>
2024-06-23 16:47:28 -0600

    complete file

--- referee/send_UDP.py
+++ referee/send_UDP.py
@@ -1,11 +1,21 @@
 import socket
-UDP_IP = "192.168.56.1"
+import struct
+UDP_IP = "127.0.0.1"
 
 UDP_PORT_answer = 3939
+
+
+header ="RGrt"
+protocol_v = 12
+team = 5
+player = 1
+message = 3
+
+data_packed = struct.pack('4s4B', header.encode(),protocol_v,team,player,message)
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((UDP_IP,UDP_PORT_answer))
 
 while True:
-    sock.sendto(b"RGrt",("192.168.56.1", UDP_PORT_answer))
-    print("funciona")
+    sock.sendto(data_packed,(UDP_IP, UDP_PORT_answer))
+    print(data_packed)
