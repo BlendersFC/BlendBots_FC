a04a7afb081228bef3935b1539de33e49cdfa661
Vicente <vicenntte@outlook.es>
2024-07-15 07:53:14 -0600

    changed data format

--- referee/send_UDP.py
+++ referee/send_UDP.py
@@ -4,18 +4,25 @@ UDP_IP = "127.0.0.1"
 
 UDP_PORT_answer = 3939
 
+# Definición de los datos según la estructura
+header = b'RGrt'    # Header "RGrt"
+version = 2         # Versión de la estructura de datos
+team = 1            # Número de equipo
+player = 5          # Número de jugador
+message = 1         # Mensaje (0: GAMECONTROLLER_RETURN_MSG_ALIVE, 1: GAMECONTROLLER_RETURN_MSG_MAN_PENALISE, 2: GAMECONTROLLER_RETURN_MSG_MAN_UNPENALISE)
+#time = 12345        # Tiempo en segundos desde el comienzo del medio
 
-header ="RGrt"
-protocol_v = 12
-team = 5
-player = 1
-message = 3
+# Empaquetar los datos en formato binario utilizando struct
+packed_data = struct.pack('<4sBBBB', header, version, team, player, message)
 
-data_packed = struct.pack('4s4B', header.encode(),protocol_v,team,player,message)
+# Crear un socket UDP
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind((UDP_IP,UDP_PORT_answer))
+# Dirección y puerto del receptor
+server_address = ('localhost', 3939)
 
 while True:
-    sock.sendto(data_packed,(UDP_IP, UDP_PORT_answer))
-    print(data_packed)
+    # Enviar los datos empaquetados
+    udp_socket.sendto(packed_data, server_address)
+    print("Datos enviados")
+
